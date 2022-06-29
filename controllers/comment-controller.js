6b7db092cdce11c6415996427a3a7d1099c10d2d
nbross <nbross2001@gmail.com>
2022-06-29 01:07:55 -0400

    added validation

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -24,7 +24,11 @@ const commentController = {
 
     // add reply to comment
     addReply({ params, body }, res) {
-        Comment.findOneAndUpdate({ _id: params.commentId }, { $push: { replies: body } }, { new: true })
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true, runValidators: true }
+        )
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
@@ -58,16 +62,16 @@ const commentController = {
             .catch(err => res.json(err));
     },
 
-// remove reply
-removeReply({ params }, res) {
-    Comment.findOneAndUpdate(
-        { _id: params.commentId },
-        { $pull: { replies: { replyId: params.replyId } } },
-        { new: true }
-    )
-        .then(dbPizzaData => res.json(dbPizzaData))
-        .catch(err => res.json(err));
-}
+    // remove reply
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => res.json(err));
+    }
 };
 
 module.exports = commentController;
